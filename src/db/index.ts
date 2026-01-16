75066394d4f607d77dec8d3c811104cbef3043ca
mac <mac@macs-MacBook-Pro.local>
2026-01-16 20:32:52 +0500

    fix db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,9 @@
 import { MongoClient } from "mongodb";
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
+const MONGODB_URI = process.env.MONGO_DB_URI_MONGODB_URI as string;
 if (!MONGODB_URI) {
-    throw new Error("❌ Please define MONGODB_URI in .env");
+    throw new Error("❌ Please define MONGO_DB_URI_MONGODB_URI in .env");
 }
 
 
