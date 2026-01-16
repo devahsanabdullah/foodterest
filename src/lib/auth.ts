23d28fcffad4a595bf387a78763c38d1d3f608ba
mac <mac@macs-MacBook-Pro.local>
2026-01-16 20:42:40 +0500

    fix db client

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { nextCookies } from "better-auth/next-js";
 
-const client = new MongoClient("mongodb://localhost:27017/foodterest");
+const client = new MongoClient(process.env.MONGO_DB_URI_MONGODB_URI as string);
 const db = client.db();
 
 export const auth = betterAuth({
@@ -13,6 +13,6 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
-    
+
     plugins: [nextCookies()]
 });
